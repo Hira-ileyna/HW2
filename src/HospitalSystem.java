6f9fb22ae0bdeb49b8d2dcfe4a307f90ab7eee50
Hiranur İleyna Bilir <hiranurbilir0@gmail.com>
2025-12-24 15:15:30 +0300

    unit commit

--- src/HospitalSystem.java
+++ src/HospitalSystem.java
@@ -21,87 +21,87 @@ public class HospitalSystem {
         if (request.isPriority()) {                                                     //If patient is priority
             priorityQueue.enqueue(request);                                             //Add to priority queue.
             System.out.println("Added priority treatment request: Patient ID: " + request.getPatientId());
-        } else {
-            treatmentQueue.enqueue(request);
+        } else {                                                                        //If patient isn't priority.
+            treatmentQueue.enqueue(request);                                            //Add to treatment queue.
             System.out.println("Added treatment request: Patient ID: " + request.getPatientId());
             }
         }
-    public void addDischargeRecord(DischargeRecord dischargerecord){                             //
-        if(!patientMap.containsKey(dischargerecord.getPatientId())){                              //
+    public void addDischargeRecord(DischargeRecord dischargerecord){                     //This method is that adds to discharge record.
+        if(!patientMap.containsKey(dischargerecord.getPatientId())){                     //This case checks if this patient is present.
             System.out.println("Didn't add discharge record: Patient ID: " + dischargerecord.getPatientId() + " not found");
-            return;                                                                     //
+            return;                                                                      //If patient isn't here. Leave from the method.
         }
-        dischargeStack.push(dischargerecord);                                                    //
+        dischargeStack.push(dischargerecord);                                            //Add to discharge record to the stack.
         System.out.println("Added Discharge record: Patient ID: " + dischargerecord.getPatientId());
     }
 
     public void processTreatmentRequest(){
-        TreatmentRequest request = priorityQueue.dequeue();
-        if (request == null) {
-            request = treatmentQueue.dequeue();
-            return;
+        TreatmentRequest request = priorityQueue.dequeue();                              //Firstly, check priority patients and remove from priority queue.
+        if (request == null) {                                                           //If, priority queue is null
+            request = treatmentQueue.dequeue();                                          //we will remove from treatment queue.
         }
-        if(request == null){
-            System.out.println("No treatment request to process.");
+        if(request == null){                                                             //If both of queues is null,
+            System.out.println("No treatment request to process.");                      //No treatment request to process.
+            return;                                                                      //If there is no request, leave from method.
         }
-        Patient patient = patientMap.get(request.getPatientId());
-        if (patient == null) {
+        Patient patient = patientMap.get(request.getPatientId());                        //Find the patient that in the request.
+        if (patient == null) {                                                           //If there is no patient, Print that the patient is not present.
             System.out.println("Patient not found for request: " + request.getPatientId());
             return;
         }
-        System.out.println("Processed treatment request: " + request);
+        System.out.println("Processed treatment request: " + request.toString());         //Report to that processed request.
     }
 
-    public static void mergeSortBySeverity(Patient[] array){                            //
-        if(array == null || array.length <= 1)                                          //
-            return;                                                                     //
-        Patient[] temp = new Patient[array.length];                                     //
-        sort(array, temp, 0, array.length - 1);                               //
+    public static void mergeSortBySeverity(Patient[] array){                              //Merge Sort sorts patients in descending order according to their severity.
+        if(array == null || array.length <= 1)                                            //If array is null or singly element
+            return;                                                                       //Return. Because there's nothing to list.
+        Patient[] temp = new Patient[array.length];                                       //We define a temporary array to use during the merge.
+        sort(array, temp, 0, array.length - 1);                                 //
     }
-    private static void sort(Patient[] array, Patient[] temp, int low, int high){       //
-        if(low >= high){                                                                //
-            return;                                                                     //
+    private static void sort(Patient[] array, Patient[] temp, int low, int high){         //This method performs the divide and sort part of the merge sort.
+        if(low >= high){                                                                  //If low == high , this array has a singly element, Also if low > high , this array has empty space.
+            return;                                                                       //This situtaion is that happened base-case.
         }
-        int mid = low + (high - low)/2;                                                 //
-        sort(array, temp, low, mid);                                                    //
-        sort(array, temp, mid + 1, high);                                          //
-        merge(array, temp, low, mid, high);                                             //
+        int mid = low + (high - low)/2;                                                   //Middle point
+        sort(array, temp, low, mid);                                                      //Sort the left half of the list
+        sort(array, temp, mid + 1, high);                                            //Sort the right half of the list
+        merge(array, temp, low, mid, high);                                               //Combine the left half and right half of the list.
     }
-    private static void merge(Patient[] array, Patient[] temp, int low, int mid, int high){ //
-        int i = low;
-        int j = mid + 1;
-        int k = low; // low’dan başlamalı
-        while (i <= mid && j <= high) {
-            if (array[i].getSeverity() >= array[j].getSeverity()) { // azalan
-                temp[k++] = array[i++];
-            } else {
-                temp[k++] = array[j++];
+    private static void merge(Patient[] array, Patient[] temp, int low, int mid, int high){ //This method combines patients according to the severity of their condition.
+        int i = low;                                                                        //Beginning of the left array.
+        int j = mid + 1;                                                                    //Beginning of the right array.
+        int k = low;                                                                        //Beginning of the temporary array. Start to low.
+        while (i <= mid && j <= high) {                                                     //Compare when both sides have elements.
+            if (array[i].getSeverity() >= array[j].getSeverity()) {                         //Since we are sorting in descending order, we need to write the greater than or equal to elements first.
+                temp[k++] = array[i++];                                                     //If we chose letf, we should write to chosing element to the temp[k++].
+            } else {                                                                        //If we chose letf,
+                temp[k++] = array[j++];                                                     //we should write to chosing element to the temp[k++].
             }
         }
-        while (i <= mid) {
-            temp[k++] = array[i++];
+        while (i <= mid) {                                                                  //If the right side is finished first,
+            temp[k++] = array[i++];                                                         //Everything on the left side should be copied to the template so that it's in the correct position.
         }
-        while (j <= high) {
-            temp[k++] = array[j++];
+        while (j <= high) {                                                                 //If the left side is finished first
+            temp[k++] = array[j++];                                                         //We copy the remaining parts of the right side.
         }
-        for (int x = low; x <= high; x++) { // doğru aralık
+        //Here we perform copy without comparison because the sub-arrays are already sorted within themselves.
+        for (int x = low; x <= high; x++) {                                                 //Once the merge operation is complete, it copies the resulting ordered sub-range from the temporary array back to the original array.
             array[x] = temp[x];
         }
-
     }
-    public void printHospitalSystem(){                                                      //
+    public void printHospitalSystem(){                                                      //Print to all of datas in the system.
         System.out.println("Hospital System");
-        patientList.printList();
+        patientList.printList();                                                            //Print the patient list
         System.out.println("Treatment Queues: ");
-        treatmentQueue.printQueue();
+        treatmentQueue.printQueue();                                                        //Print the treatment queue.
         System.out.println("Priority Queues: ");
-        priorityQueue.printQueue();
+        priorityQueue.printQueue();                                                         //print the priority queue.
         System.out.println("Discharge Queues: ");
-        dischargeStack.printStack();
-        System.out.println("Sorted Patients by Severtiy: ");
-        Patient[] array = patientList.toArray();                                          //
-        HospitalSystem.mergeSortBySeverity(array);                                        //
-        for(int i = 0; i < array.length; i++){
+        dischargeStack.printStack();                                                        //print the discharge stack
+        System.out.println("Sorted Patients by Severity: ");
+        Patient[] array = patientList.toArray();                                            //Convert the patients on the linked list into an array.
+        HospitalSystem.mergeSortBySeverity(array);                                          //Sort the array in descending order of severity.
+        for(int i = 0; i < array.length; i++){                                              //Print the sorted array.
             System.out.println(array[i]);
         }
     }
